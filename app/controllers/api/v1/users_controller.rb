dd9c6d3293e539a5ed51e90c9a66395e6639ca05
bertil291utn <bertilt4@gmail.com>
2020-10-28 16:05:18 -0500

    Adds the user create endpoint

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,20 @@
 class Api::V1::UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
   def show
     render json: User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
   end
 end
--- spec/requests/user_request_spec.rb
+++ spec/requests/user_request_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Users', type: :request do
   let(:usuario) { create :user }
   describe 'should show user' do
-    it 'returns succes as http status response' do
+    it 'returns success as http status response' do
       get "/api/v1/users/#{usuario.id}"
       expect(response).to have_http_status(:success)
     end
@@ -12,6 +12,18 @@ RSpec.describe 'Users', type: :request do
       get "/api/v1/users/#{usuario.id}"
       json_response = JSON.parse(response.body)
       expect(json_response['email']).to match(usuario.email)
+    end
+  end
+
+  describe 'create user' do
+    it 'returns success as http status response ' do
+      post '/api/v1/users', params: { user: { name: 'newuser', email: 'new@email.com', password: 'password' } }
+      expect(response).to have_http_status(:created)
+    end
+
+    it 'returns unprocessale entity' do
+      post '/api/v1/users', params: { user: { name: usuario.name, email: usuario.email, password: usuario.password_digest } }
+      expect(response).to have_http_status(:unprocessable_entity)
     end
   end
 end
