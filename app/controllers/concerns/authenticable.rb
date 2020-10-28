a8d7fa6b366f840c8da3c0983c1be436a43917c7
bertil291utn <bertilt4@gmail.com>
2020-10-28 17:17:48 -0500

    Adds authenticable module for managing authetication methods

---
+++ app/controllers/concerns/authenticable.rb
@@ -0,0 +1,16 @@
+module Authenticable
+  def current_user
+    return @current_user if @current_user
+
+    header = request.headers['Authorization']
+    return nil if header.nil?
+
+    decoded = JsonWebToken.decode(header)
+    begin
+      @current_user = User.find(decoded[:user_id])
+    rescue ActiveRecord::RecordNotFound
+      @current_user = nil
+    end
+    @current_user
+  end
+end
