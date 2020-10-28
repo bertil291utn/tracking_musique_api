12dd0d1358f1414e1dedc1961f71c4c9f904852b
bertil291utn <bertilt4@gmail.com>
2020-10-28 17:22:34 -0500

    Set user to show

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -9,10 +11,14 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def show
-    render json: User.find(params[:id])
+    render json: @user
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:name, :email, :password)
