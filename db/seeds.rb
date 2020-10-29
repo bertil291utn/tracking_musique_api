df709484f705564109bf25026df2c28c76b26e3c
bertil291utn <bertilt4@gmail.com>
2020-10-29 15:33:09 -0500

    Creat a seed to populate DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Artist.delete_all
+User.delete_all
+ids = %w[003vvx7Niy0yvhvHt4a68B 0C0XlULifJtAgn6ZNCW2eu 1sTsuZTdANkiFd7T34H3nb 7cX4PJz1old9fyFI8RlfgW]
+
+5.times do
+  name = Faker::Name.name
+  user = User.create! name: name, email: Faker::Internet.email(name: name), password: name[0] + '123456'
+
+  ids.each do |elem|
+    Artist.create!(
+      id_string: elem,
+      user_id: user.id,
+    )
+  end
+end
