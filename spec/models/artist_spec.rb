c493c4a6f781e84a1ed5313f1c90db4d1821aa2a
bertil291utn <bertilt4@gmail.com>
2020-10-29 09:54:44 -0500

    Validations to artist

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Artist, type: :model do
   subject { create :artist }
+  describe 'associations' do
+    it { should belong_to(:user) }
+  end
+
   describe 'validations' do
     it { should validate_length_of(:id_string).is_at_most(22) }
     it { should allow_value('6U5ySz3l4LtpaJ37pr3bGU').for(:id_string) }
