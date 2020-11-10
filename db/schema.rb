2b0e99d48108509e550a444d8432c39a2f49b726
bertil291utn <bertilt4@gmail.com>
2020-11-10 16:28:35 -0500

    Add new column to artis - Add to save on DB and not request always  on the frontend

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_09_232828) do
+ActiveRecord::Schema.define(version: 2020_11_10_212123) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2020_11_09_232828) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.string "photoUrl"
     t.index ["id_string", "user_id"], name: "index_artists_on_id_string_and_user_id", unique: true
     t.index ["user_id"], name: "index_artists_on_user_id"
   end
