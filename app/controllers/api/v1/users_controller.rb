a66faaab224b37775d3e5409b1b67d64bc399575
bertil291utn <bertilt4@gmail.com>
2020-11-14 09:20:18 -0500

    Add new var - Userparams and token separate variables

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,7 +4,9 @@ class Api::V1::UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      render json: { user: { id: user.id, name: user.name, email: user.email }, token: JsonWebToken.encode(user_id: user.id) }, status: :created
+      userparams = { id: user.id, name: user.name, email: user.email }
+      token = JsonWebToken.encode(user_id: user.id)
+      render json: { user: userparams, token: token }, status: :created
     else
       render json: user.errors, status: :unprocessable_entity
     end
