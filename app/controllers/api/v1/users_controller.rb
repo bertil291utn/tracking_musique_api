7c2bf4b00579ec1a96e6ec79cce72aa97d1513d9
bertil291utn <bertilt4@gmail.com>
2020-11-14 09:15:29 -0500

    Add token params - After create a user return a token - Solve issue to avoid call again token controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,11 +2,11 @@ class Api::V1::UsersController < ApplicationController
   before_action :set_user, only: %i[show update destroy user_artists]
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      render json: UserSerializer.new(@user).serializable_hash, status: :created
+    user = User.new(user_params)
+    if user.save
+      render json: { user: { id: user.id, name: user.name, email: user.email }, token: JsonWebToken.encode(user_id: user.id) }, status: :created
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: user.errors, status: :unprocessable_entity
     end
   end
 
